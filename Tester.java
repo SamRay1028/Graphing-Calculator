3b7c0348257ba44193eb750689fedba01458c53d
Samuel Ard <samraymondard@gmail.com>
2024-10-03 13:32:17 -0400

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Tester {
     public static void main(String[] args){
-        JFrame window = new JFrame("Cards?");
+        JFrame window = new JFrame("Graphing Calculator");
         window.setSize(1001, 1026);
         window.add(new Window());
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
